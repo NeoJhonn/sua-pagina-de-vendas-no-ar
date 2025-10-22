6cf63d8dcd838e05b0bdb37832242e9554bd71c4
NeoJhonn <jhonny.azevedo@gmail.com>
2025-10-22 01:43:47 -0300

    Adde Dynamic sitemap generatio7

--- src/scripts/generate-sitemap.js
+++ src/scripts/generate-sitemap.js
@@ -8,7 +8,7 @@ const urls = [
 ];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.join('\n')}
 </urlset>`;
 
