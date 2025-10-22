dd2060005e643e222f980fd8fbe3f26095a0711b
NeoJhonn <jhonny.azevedo@gmail.com>
2025-10-22 01:29:36 -0300

    Adde Dynamic sitemap generation2

--- src/scripts/generate-sitemap.js
+++ src/scripts/generate-sitemap.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import path from 'path';
 
 const baseUrl = 'https://sua-pagina-de-vendas-no-ar.vercel.app/';
 
@@ -7,7 +8,7 @@ const urls = [
 ];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.join('\n')}
 </urlset>`;
 
